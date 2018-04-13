4dfc63f408a8c50c6c1310847a75d5a26e17de63
liujian <694580957@qq.com>
2018-04-13 11:32:17 +0800

    cache

--- app/component/index/index.js
+++ app/component/index/index.js
@@ -1,44 +1,9 @@
 import React from 'react';
-import '../../public/css/index.pcss';
+import '../../public/css/index.pcss'
+import Seconds from '../common/Seconds'
 
-class Index extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { seconds: 0 };
-    }
-
-    tick() {
-        this.setState(prevState => ({
-            seconds: prevState.seconds + 1
-        }));
-    }
-
-    componentDidMount() {
-        this.interval = setInterval(() => this.tick(), 1000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.interval);
-    }
-
-    render() {
-        return (
-            <div className="cont">
-                <div className="top">
-                    <div>这是头部1</div>
-                    <i className="logo"/>
-
-                </div>
-                <div className="bottom">
-                    这是底部2
-                </div>
-                <div>
-                    Seconds: {this.state.seconds}
-                </div>
-            </div>
-
-        );
-    }
-}
+const Index = () =>
+        <Seconds title="首页"/>
+    ;
 
 export default Index;
